f8f157677b374cc5b614fc93ea4748e4c5fb61ff
Ketevann <ketevan.tsin@gmail.com>
2018-02-26 15:39:09 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { ants: [], allTests: 'not started', counter: 0 }
+    this.state = { ants: [], allTests: 'not started', counter: 0 };
     this.calculate = this.calculate.bind(this);
     this.callback = this.callback.bind(this);
   }
